f5ac3e2986d981dcb97f24ce24744e1e7314657d
themostdavid <38319257+themostdavid@users.noreply.github.com>
2018-04-14 17:23:17 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-var gameBoard =
+var game =
 {
-	//dimensions of game board
-	width : 0,
-	height : 0,
+    gameBoard:
+    {
+        //dimensions of game board
+        width : 0,
+        height : 0,
+        
+        //Game board array
+        board : []
+    },
 
-	//Funny h meme
-	hJ : prompt("Type a funny meme", "")
-}
+    nFlags : 100
+};
 
-
-
-
-var a = 0;
-var b = 0;
-function changeParagraph() 
+var cell =
 {
-	if(a == 0)
-    {
-    	document.getElementById("demo").innerHTML = "Paragraph changed.";
-        a = 1;
-    }
-    else
-    {
-    	document.getElementById("demo").innerHTML = "A Paragraph.";
-        a = 0;
-    }
-}
-function changeTitle()
+    //0 means safe, 1 means mine
+    mineStatus : 0,
+
+    //0 means hidden, 1 means revealed
+    visualStatus : 0,
+};
+
+//Changes value of flag count
+function changeFlagCount(x)
 {
-	if(b == 0)
+    if((game.nFlags > 0) && (game.nFlags < 100))
     {
-    	document.getElementById("title").innerHTML = gameBoard.hJ;
-        //document.getElementById("title").style.display = "none";
-        b = 1;
+        game.nFlags += x;
+        document.getElementById("nFlags").innerHTML = "Number of flags: " + game.nFlags;
     }
-    else
+    else if((game.nFlags == 100) && (x == -1))
     {
-    	document.getElementById("title").innerHTML = "JavaScript in Head.";
-        document.getElementById("title").style.display = "block";
-        b = 0;
+        game.nFlags += x;
+        document.getElementById("nFlags").innerHTML = "Number of flags: " + game.nFlags;
+    }
+    else if((game.nFlags == 0) && (x == 1))
+    {
+        game.nFlags += x;
+        document.getElementById("nFlags").innerHTML = "Number of flags: " + game.nFlags;
     }
 }
